d55a557b4b2fe24bbea4c0f71455d4ee03e40ec5
Nick Lee <raven1300@hotmail.com>
2021-09-26 14:26:41 +1000

    added routing for creating and adjusting workouts

---
+++ routes/workouts.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { modelNames } = require("mongoose");
+const router = express.Router();
+const db = require("../models");
+
+// Get all workouts
+router.get("/", async (req, res) => {
+    try {
+        const workouts = await db.Workout.find({});
+        res.json(workouts);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// Get range
+router.get("/range", async (req, res) => {
+    try {
+        const workouts = await db.Workout.find({});
+        res.json(workouts);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// create new workout
+router.post("/", async (req, res) => {
+    try {
+        const workout = req.body;
+        workout.day = Date.now();
+        const result = await db.Workout.create(workout);
+        res.json(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+// add to existing workout
+router.put("/:id", async (req, res) => {
+    try {
+        const exercise = req.body;
+        const result = await db.Workout.findByIdAndUpdate(
+            req.params.id,
+            { $push: { exercises: exercise } },
+            { new: true }
+        );
+        res.json(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router;
